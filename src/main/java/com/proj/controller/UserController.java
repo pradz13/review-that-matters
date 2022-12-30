e491fae77f6f791f28cdbc293e9c9e6eb83cf8e8
Pradipta <pradipta.nag2@gmail.com>
2022-12-30 20:57:15 +0530

    Introduction of Spring Security

---
+++ src/main/java/com/proj/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.proj.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @RequestMapping("/dashboard")
+    public String dashboard() {
+        return "normal/user_dashboard";
+    }
+}
