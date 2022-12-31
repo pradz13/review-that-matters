7ed893638021ab89c381a503cb439ad844d3f833
Pradipta <pradipta.nag2@gmail.com>
2022-12-31 11:23:20 +0530

    Spring Security - User Dashboard post login

--- src/main/java/com/proj/controller/UserController.java
+++ src/main/java/com/proj/controller/UserController.java
@@ -1,14 +1,26 @@
 package com.proj.controller;
 
+import com.proj.entities.User;
+import com.proj.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @RequestMapping("/dashboard")
-    public String dashboard() {
+    public String dashboard(Model model, Principal principal) {
+        String userName = principal.getName();
+        User user = this.userRepository.getUserByEmail(userName);
+        model.addAttribute("user", user);
         return "normal/user_dashboard";
     }
 }
