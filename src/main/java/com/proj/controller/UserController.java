f3599bf24633e2867a708a134e0d84a7e338516c
Pradipta <pradipta.nag2@gmail.com>
2022-12-31 14:47:02 +0530

    User dashboard - Add Post - Intermediate Commit

--- src/main/java/com/proj/controller/UserController.java
+++ src/main/java/com/proj/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.proj.controller;
 
+import com.proj.entities.Post;
 import com.proj.entities.User;
 import com.proj.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
@@ -16,11 +18,22 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @RequestMapping("/dashboard")
-    public String dashboard(Model model, Principal principal) {
+    @ModelAttribute
+    public void addCommonData(Model model, Principal principal) {
         String userName = principal.getName();
         User user = this.userRepository.getUserByEmail(userName);
         model.addAttribute("user", user);
+    }
+
+    @RequestMapping("/dashboard")
+    public String dashboard(Model model, Principal principal) {
         return "normal/user_dashboard";
+    }
+
+    @RequestMapping("/add-post")
+    public String openAddPostForm(Model model) {
+        model.addAttribute("title", "Add Post");
+        model.addAttribute("post", new Post());
+        return "normal/add_post";
     }
 }
